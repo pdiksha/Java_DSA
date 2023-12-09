acd38dc6f0995560c43fd4a277a70b8a74d29299
Diksha Pathak <pathakdiksha712@gmail.com>
2023-12-09 07:32:56 +0530

    Immutable Classes

--- LL/src/ImmutableClass.java
+++ LL/src/ImmutableClass.java
@@ -1,11 +1,14 @@
+import java.util.Date;
 
 final class Stud{
     private final int rollno;
     private final String name;
+    private final Date dob;
 
-    Stud(int rollno, String name) {
+    Stud(int rollno, String name, Date dob) {
         this.rollno = rollno;
         this.name = name;
+        this.dob = new Date(dob.getTime());
     }
     public String getName()
     {
@@ -15,18 +18,20 @@ final class Stud{
     {
         return this.rollno;
     }
+    public Date getDob() { return new Date(dob.getTime()); }
 }
 
 
 public class ImmutableClass {
     public static void main(String[] args) {
-        Stud s = new Stud(1, "Diksha");
+        Stud s = new Stud(1, "Diksha", new Date());
         System.out.println(s.getName());
         System.out.println(s.getRollno());
+        System.out.println(s.getDob());
 
         //class should be final
         //since there will be no setter methods we will not be setting any value
-        //we also cannot directly access data members becase they are private
+        //we also cannot directly access data members because they are private
         //cannot change their values because they are final
         //https://www.geeksforgeeks.org/create-immutable-class-java/
     }
