be4bd79da85e621ce934f6f16243e2028e06d0f6
Diksha Pathak <pathakdiksha712@gmail.com>
2023-12-16 06:46:39 +0530

    binary tree

--- LL/src/LearnTree.java
+++ LL/src/LearnTree.java
@@ -81,6 +81,54 @@ public class LearnTree {
         }
     }
 
+    public static int countOfNodes(Node root){
+
+        if(root == null){
+            return 0;
+        }
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+        return leftNodes+rightNodes+1;
+    }
+
+    public static int sumOfNodes(Node root){
+
+        if(root == null){
+            return 0;
+        }
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        return leftSum+rightSum+root.data;
+    }
+
+    public static int height(Node root){
+
+        if(root==null){
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        int myHeight = Math.max(leftHeight, rightHeight)+1;
+        return myHeight;
+    }
+
+    public static int diameter(Node root){
+
+        if(root==null){
+            return 0;
+        }
+
+        int diam1 = diameter(root.left);
+        int diam2 = diameter(root.right);
+        int diam3 = height(root.left)+ height(root.right) +1;
+
+        return Math.max(diam3, Math.max(diam1, diam2));
+
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -99,6 +147,14 @@ public class LearnTree {
         System.out.println();
         System.out.println();
         levelOrder(root);
+
+        System.out.println();
+        System.out.println(countOfNodes(root));
+        System.out.println(sumOfNodes(root));
+
+        System.out.println(height(root));
+
+        System.out.println(diameter(root));
 
 
 
--- LL/src/TestDemo.java
+++ LL/src/TestDemo.java
@@ -4,6 +4,19 @@ import java.util.List;
 public class TestDemo {
 
     public static void main(String[] args) {
+        List<List<Integer>> list1 = new ArrayList<>();
+
         List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list1.add(list);
+
+        list2.add(3);
+        list2.add(4);
+        list1.add(list2);
+        System.out.println(list1);
+
     }
 }
